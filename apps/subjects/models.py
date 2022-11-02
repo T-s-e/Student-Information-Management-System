5099e6609fb598902924786b1d286fe80cfc6f38
ku1z <110317064+ku1z@users.noreply.github.com>
2022-11-02 11:59:18 +0800

    Update models.py

--- apps/subjects/models.py
+++ apps/subjects/models.py
@@ -19,23 +19,11 @@ class Subject(models.Model):
     credit = models.CharField(max_length=200, unique=True)
     subject = models.CharField(max_length=200)
     professor = models.CharField(max_length=200)
-    # online_source = models.CharField(max_length=200, blank=True)
     online_source = models.URLField(max_length=200)
     property = models.CharField(max_length=10, choices=PROPERTY_CHOICES, default="必修")
     date_of_test = models.DateField(default=timezone.now)
-    # current_class = models.ForeignKey(
-    #     SubjectClass, on_delete=models.SET_NULL, blank=True, null=True
-    # )
-    # date_of_admission = models.DateField(default=timezone.now)
 
-    # mobile_num_regex = RegexValidator(
-    #     regex="^[0-9]{10,15}$", message="Entered mobile number isn't in a right format!"
-    # )
-    # parent_mobile_number = models.CharField(
-    #     validators=[mobile_num_regex], max_length=13, blank=True
-    # )
 
-    # address = models.TextField(blank=True)
     others = models.TextField(blank=True)
     picture = models.ImageField(blank=True, upload_to="subjects/pictures/")
 
@@ -48,7 +36,7 @@ class Subject(models.Model):
     def get_absolute_url(self):
         return reverse("subject-detail", kwargs={"pk": self.pk})
 
-
+#用于学科下载
 class SubjectBulkUpload(models.Model):
     date_uploaded = models.DateTimeField(auto_now=True)
     csv_file = models.FileField(upload_to="subjects/bulkupload/")
